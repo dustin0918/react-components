1e716510ec7d32e8748d6bcbc62cb8e6ce915fb1
dustinxie <dustinxie@hotmail.com>
2019-07-27 20:56:30 -0700

    passing multiple props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker'
 import CommentDetail from './CommentDetail'
 
 const App =()=>{
     return (
         <div className="ui container comments">
-           <CommentDetail author="sam"/>
-           <CommentDetail author="alex"/>
-           <CommentDetail author="jane"/>
+           <CommentDetail 
+           author="sam" 
+           timeAgo="today at 4:45pm" 
+           content="hi"
+           avatar={faker.image.avatar()}
+           />
+           <CommentDetail 
+           author="alex" 
+           timeAgo="today at 2:00pm" 
+           content="hello"
+           avatar={faker.image.avatar()}
+           />
+           <CommentDetail 
+           author="jane" 
+           timeAgo="yesterday at 5:00pm" 
+           content="yyoyo"
+           avatar={faker.image.avatar()}
+           />
           
         </div>
     );
