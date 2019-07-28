5229101cee54440b51817788f3305245fc86b0a9
dustinxie <dustinxie@hotmail.com>
2019-07-27 20:48:10 -0700

    passing props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+
 import CommentDetail from './CommentDetail'
 
 const App =()=>{
     return (
         <div className="ui container comments">
-           <CommentDetail/>
-           <CommentDetail/>
-           <CommentDetail/>
+           <CommentDetail author="sam"/>
+           <CommentDetail author="alex"/>
+           <CommentDetail author="jane"/>
+          
         </div>
     );
 
